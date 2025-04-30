71690861af9c20dac1165b27815851983daf94b4
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-04-29 23:49:30 -0300

    teste 1, I == 1

--- testa_romanos.cpp
+++ testa_romanos.cpp
@@ -3,18 +3,17 @@
 #include "romanos.hpp"
 
 
-TEST_CASE( "Numeros romanos - algarismos �nicos", "[romanos]" ) {
+TEST_CASE( "Numeros romanos - algarismos únicos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("I") == 1 );
     
     //REQUIRE( romanos_para_decimal("V") == 5 );
 
-    
 }
 
 
 
 /*
-TEST_CASE( "Numeros romanos - algarismos inv�lidos", "[romanos]" ) {
+TEST_CASE( "Numeros romanos - algarismos inválidos", "[romanos]" ) {
     REQUIRE( romanos_para_decimal("G") == -1 );
     
     REQUIRE( romanos_para_decimal("i") == -1 );
