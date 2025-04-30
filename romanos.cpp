5e0509926149238f5a6c12203a390184e51bb3e5
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-04-30 01:39:55 -0300

    teste 6 (mais algarismos invalidos) - passou

--- romanos.cpp
+++ romanos.cpp
@@ -21,16 +21,34 @@ int romanos_para_decimal(char const * num_romano)
 { 
     int decimal = 0;
     int count = 0;
-    int V_count, L_count, D_count;
+
+    // Contadores para V, L e D
+    int V_count, L_count, D_count; 
     V_count = L_count = D_count = 0;
+
+    // Contadores consecutivos para I, X, C e M
     int I_consec_count, X_consec_count, C_consec_count, M_consec_count;
     I_consec_count = X_consec_count = C_consec_count = M_consec_count = 0;
+
+    // Contadores totais para I, X, C e M
+    int I_count, X_count, C_count, M_count;
+    I_count = X_count = C_count = M_count = 0;
+
     while (num_romano[count] != '\0')
     {  
         switch (num_romano[count])
         {
 
         case 'I':
+            // Atualiza e checa contador total
+            I_count++;
+            if (I_count > 4)
+            {
+                return -1;
+            }
+
+            // Atualiza e checa contador consecutivo
+            // Se o próximo algarismo não for I, reseta o contador
             I_consec_count++;
             if (I_consec_count > 3)
             {
@@ -41,6 +59,9 @@ int romanos_para_decimal(char const * num_romano)
                 I_consec_count = 0;
             }
             
+            // Checa se o algarismo subtrai do próximo ou não
+            // Se o próximo algarismo for V ou X, subtrai 1
+            // Se não, soma 1
             if (num_romano[count + 1] == 'V' || num_romano[count + 1] == 'X')
             {
                 decimal -= 1;
@@ -53,6 +74,7 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'V':
+            // Atualiza e checa contador, V não pode ser repetido
             V_count++;
             if (V_count > 1)
             {
@@ -62,6 +84,15 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'X':
+            // Atualiza e checa contador total
+            X_count++;
+            if (X_count > 4)
+            {
+                return -1;
+            }
+
+            // Atualiza e checa contador consecutivo
+            // Se o próximo algarismo não for X, reseta o contador
             X_consec_count++;
             if (X_consec_count > 3)
             {
@@ -72,6 +103,9 @@ int romanos_para_decimal(char const * num_romano)
                 X_consec_count = 0;
             }
             
+            // Checa se o algarismo subtrai do próximo ou não
+            // Se o próximo algarismo for L ou C, subtrai 10
+            // Se não, soma 10
             if (num_romano[count + 1] == 'L' || num_romano[count + 1] == 'C')
             {
                 decimal -= 10;
@@ -84,6 +118,7 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'L':
+            // Atualiza e checa contador, L não pode ser repetido
             L_count++;
             if (L_count > 1)
             {
@@ -93,6 +128,14 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'C':
+            // Atualiza e checa contador total
+            C_count++;
+            if (C_count > 4)
+            {
+                return -1;
+            }
+            // Atualiza e checa contador consecutivo
+            // Se o próximo algarismo não for C, reseta o contador
             C_consec_count++;
             if (C_consec_count > 3)
             {
@@ -102,7 +145,10 @@ int romanos_para_decimal(char const * num_romano)
             {
                 C_consec_count = 0;
             }
-
+            
+            // Checa se o algarismo subtrai do próximo ou não
+            // Se o próximo algarismo for D ou M, subtrai 100
+            // Se não, soma 100
             if (num_romano[count + 1] == 'D' || num_romano[count + 1] == 'M')
             {
                 decimal -= 100;
@@ -115,6 +161,7 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'D':
+            // Atualiza e checa contador, D não pode ser repetido
             D_count++;
             if (D_count > 1)
             {
@@ -124,6 +171,15 @@ int romanos_para_decimal(char const * num_romano)
             break;
 
         case 'M':
+            // Atualiza e checa contador total
+            M_count++;
+            if (M_count > 4)
+            {
+                return -1;
+            }
+
+            // Atualiza e checa contador consecutivo
+            // Se o próximo algarismo não for M, reseta o contador
             M_consec_count++;
             if (M_consec_count > 3)
             {
@@ -133,11 +189,13 @@ int romanos_para_decimal(char const * num_romano)
             {
                 M_consec_count = 0;
             }
-            
+
+            // M nao subtrai de nenhum algarismo
             decimal += 1000;
             break;
         
         default:
+            // Se o algarismo não for válido, retorna -1
             return -1;
             break;
         }
@@ -146,3 +204,4 @@ int romanos_para_decimal(char const * num_romano)
     return decimal; 
 }
  
+
