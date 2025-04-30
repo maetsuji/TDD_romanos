e53fac81cd0d7182063b6ee5e3687922d9b639c3
Rodrigo Rafik <rodrigorafikmm@gmail.com>
2025-04-29 23:55:23 -0300

    teste 1 - passou

--- romanos.cpp
+++ romanos.cpp
@@ -1,6 +1,17 @@
 #include "romanos.hpp"
 int romanos_para_decimal(char const * num_romano)
 {
-  return 0; 
+  int decimal = 0;
+  switch (num_romano[0])
+  {
+  case 'I':
+    decimal = 1;
+    break;
+  
+  default:
+    return -1;
+    break;
+  }
+  return decimal; 
 }
  
